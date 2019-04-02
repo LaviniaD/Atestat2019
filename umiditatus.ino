dc21e52b05906f25f0ba9523b71f4017b06884d6
LaviniaD <32830418+LaviniaD@users.noreply.github.com>
2019-04-03 00:32:30 +0300

    Update umiditatus.ino

--- umiditatus.ino
+++ umiditatus.ino
@@ -1,61 +1,53 @@
-// Import required libraries
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 #include <Wire.h>
 #include <Arduino.h>
 
-//include the library for all the sensors exept humidity
+//the library that contains all sensors, except the Soil Humidity one
 #include "Sensors.h"
 
 // Replace with your network credentials
-const char* ssid = "UPC4747867";
-const char* password = "ffyQhrd6whtt";
+const char* ssid = "sthsth";
+const char* password = "sthsth";
 
-int i;
+
 // Soil Humidity sensor
 const int humPin = 34;
 int outHum;
-//the sensor struct -> i ll use this to send the data from the sesnors
+
 struct SensorData sensData;
 
 //the LEDs
 const int led1 = 15;
-const int led2 = 2;
+const int led2 = 25;
 String ledState;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-//string here
-String shumi;
-String sens1;
-String sens2;
-String sens3;
-String sens4;
-String sens5;
-String sens6;
-String sens7;
-
-String list[8][10] = {shumi,sens1,sens2,sens3,sens4,sens5,sens6,sens7};
-String sending[8][11]={"/readSens0","/readSens1","/readSens2","/readSens3","/readSens4","/readSens5","/readSens6","/readSens7"};
+String list[9];
 
 String processor(const String& var)
-{
-  
+{ 
   return String();
 }
 
 
 void setup() {
-  Wire.begin();
-Serial.begin(115200);
- pinMode(led1, OUTPUT);
- pinMode(led2, OUTPUT);
-digitalWrite( led1, LOW);
-digitalWrite( led2, LOW);
- //initialize the sensors here
- sensorsInit();
+  
+    Wire.begin();
+    Serial.begin(115200);
+
+    
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    digitalWrite( led1, LOW);
+    digitalWrite( led2, LOW);
+
+    
+     //initialize the sensors here
+    sensorsInit();
 
   if(!SPIFFS.begin()){
      Serial.println("An Error has occurred while mounting SPIFFS");
@@ -83,16 +75,44 @@ digitalWrite( led2, LOW);
     request->send(SPIFFS, "/index.html", "text/html", false, processor);
   });
 
-  
 
   //========================read the sensors and send the data ==============
-  for(i=0; i<=7; i++)
-  {
-    server.on(sending[i], HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plane", list[i]);
+  server.on("/readSens0", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plane", list[0]);
+  });
+
+ server.on("/readSens1", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plane", list[1]);
+  });
+
+  server.on("/readSens2", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plane", list[2]);
+  });
+
+  server.on("/readSens3", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plane", list[3]);
+  });
+
+  server.on("/readSens4", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plane", list[4]);
+  });
+
+  server.on("/readSens5", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plane", list[5]);
+  });
+
+  server.on("/readSens6", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plane", list[6]);
+  });
+
+  server.on("/readSens7", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plane", list[7]);
   });
   
-  // Route to set GPIO to HIGH
+  
+
+
+  // Route to set GPIO to HIGH/LOW
   server.on("/on1", HTTP_GET, [](AsyncWebServerRequest *request){
     digitalWrite(led1, HIGH);    
     request->send(SPIFFS, "/index.html", String(), false, processor);
@@ -112,23 +132,30 @@ digitalWrite( led2, LOW);
     digitalWrite(led2, LOW);    
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
+
+ 
   
   server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
+
+
   outHum= analogRead(humPin);
   outHum = map(outHum,4095,0,0,100);
   sensData = sensorsRead();
 
-  shumi=String(outHum);
-  sens1=String(sensData.ShT31D.temp);
-  sens2=String(sensData.ShT31D.humi);
-  sens3=String(sensData.GY30);
-  sens4=String(sensData.mq5);
-  sens5=String(sensData.mq135);
-  sens6=String(sensData.ms5611.pressure);
-  sens7=String(sensData.ms5611.altitude);
-
+  list[0]=String(outHum);
+  list[1]=String(sensData.ShT31D.temp);
+  list[2]=String(sensData.ShT31D.humi);
+  list[3]=String(sensData.GY30);
+  list[4]=String(sensData.mq5);
+  list[5]=String(sensData.mq135);
+  list[6]=String(sensData.ms5611.pressure);
+  list[7]=String(sensData.ms5611.altitude);
+   if(list[3].toInt()>300) //there's enough light
+  {
+    digitalWrite(led2, LOW);    
+    digitalWrite(led1, LOW);  
+  }
 }
